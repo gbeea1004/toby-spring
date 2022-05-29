d758600dc16b50e433f7e3ed1e1d9c106c94c0e8
my <jvckigh@gmail.com>
2022-05-30 00:44:50 +0900

    UserDaoTest 픽스처 추가

--- src/test/java/geon/hee/tobyspring/repository/UserDaoTest.java
+++ src/test/java/geon/hee/tobyspring/repository/UserDaoTest.java
@@ -16,53 +16,50 @@ import static org.assertj.core.api.Assertions.assertThat;
 class UserDaoTest {
 
     private UserDao userDao;
+    private User user1;
+    private User user2;
+    private User user3;
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws SQLException {
         ApplicationContext ac = new AnnotationConfigApplicationContext(DaoFactory.class);
         userDao = ac.getBean("userDao", UserDao.class);
+
+        user1 = new User("userA", "성건희", "12341234!");
+        user2 = new User("userB", "지코", "asdf!");
+        user3 = new User("userC", "재키와이", "evz2!");
+
+        userDao.deleteAll();
+        assertThat(userDao.getCount()).isEqualTo(0);
     }
 
     @Test
     void add() throws SQLException {
         // given
-        User user = new User("geonhee", "성건희", "12341234!");
 
         // when
-        userDao.add(user);
-        User findUser = userDao.get(user.getId());
+        userDao.add(user1);
+        User findUser = userDao.get(user1.getId());
 
         // then
-        assertThat(findUser.getId()).isEqualTo(user.getId());
-        assertThat(findUser.getName()).isEqualTo(user.getName());
-        assertThat(findUser.getPassword()).isEqualTo(user.getPassword());
+        assertThat(findUser.getId()).isEqualTo(user1.getId());
+        assertThat(findUser.getName()).isEqualTo(user1.getName());
+        assertThat(findUser.getPassword()).isEqualTo(user1.getPassword());
     }
 
     @Test
     void addAndGet() throws SQLException {
-        userDao.deleteAll();
-        assertThat(userDao.getCount()).isEqualTo(0);
-
-        User user = new User("geonhee", "성건희", "12341234!");
-
-        userDao.add(user);
+        userDao.add(user1);
         assertThat(userDao.getCount()).isEqualTo(1);
 
-        User findUser = userDao.get(user.getId());
-        assertThat(findUser.getId()).isEqualTo(user.getId());
-        assertThat(findUser.getName()).isEqualTo(user.getName());
-        assertThat(findUser.getPassword()).isEqualTo(user.getPassword());
+        User findUser = userDao.get(user1.getId());
+        assertThat(findUser.getId()).isEqualTo(user1.getId());
+        assertThat(findUser.getName()).isEqualTo(user1.getName());
+        assertThat(findUser.getPassword()).isEqualTo(user1.getPassword());
     }
 
     @Test
     void count() throws SQLException {
-        User user1 = new User("userA", "성건희", "12341234!");
-        User user2 = new User("userB", "지코", "asdf!");
-        User user3 = new User("userC", "재키와이", "evz2!");
-
-        userDao.deleteAll();
-        assertThat(userDao.getCount()).isEqualTo(0);
-
         userDao.add(user1);
         assertThat(userDao.getCount()).isEqualTo(1);
 
@@ -74,10 +71,7 @@ class UserDaoTest {
     }
 
     @Test
-    void getUserFailure() throws SQLException {
-        userDao.deleteAll();
-        assertThat(userDao.getCount()).isEqualTo(0);
-
+    void getUserFailure() {
         Assertions.assertThatThrownBy(() -> userDao.get("unknown_id"))
                   .isInstanceOf(EmptyResultDataAccessException.class);
     }
