3f8f6883be9079f1098b50936fb35dd7b3eacf9a
my <jvckigh@gmail.com>
2022-05-31 17:27:05 +0900

    계산기 메서드 템플릿 콜백 패턴으로 변경

--- src/test/java/geon/hee/tobyspring/study/Calculator.java
+++ src/test/java/geon/hee/tobyspring/study/Calculator.java
@@ -7,23 +7,30 @@ import java.io.IOException;
 public class Calculator {
 
     public Integer calcSum(String filePath) throws IOException {
-        BufferedReader br = null;
-
-        try {
-            br = new BufferedReader(new FileReader(filePath));
+        return fileReadTemplate(filePath, br -> {
             Integer sum = 0;
             String line = null;
             while ((line = br.readLine()) != null) {
                 sum += Integer.parseInt(line);
             }
             return sum;
+        });
+    }
+
+    private Integer fileReadTemplate(String filePath, BufferedReaderCallback callback) throws IOException {
+        BufferedReader br = null;
+
+        try {
+            br = new BufferedReader(new FileReader(filePath));
+            return callback.doSomethingWithReader(br);
         } catch (IOException e) {
             throw e;
         } finally {
             if (br != null) {
                 try {
                     br.close();
-                } catch (IOException e) {}
+                } catch (IOException e) {
+                }
             }
         }
     }
