fd60afb8138860021aa89a28cfb741b3b6b533c9
my <jvckigh@gmail.com>
2022-06-02 19:59:07 +0900

    JdbcTemplate 사용하도록 변경

--- src/main/java/geon/hee/tobyspring/repository/UserDao.java
+++ src/main/java/geon/hee/tobyspring/repository/UserDao.java
@@ -1,22 +1,16 @@
 package geon.hee.tobyspring.repository;
 
 import geon.hee.tobyspring.domain.User;
-import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 public class UserDao {
 
-    private final JdbcContext jdbcContext;
-    private final DataSource dataSource;
+    private final JdbcTemplate jdbcTemplate;
 
     public UserDao(DataSource dataSource) {
-        this.jdbcContext = new JdbcContext(dataSource);
-        this.dataSource = dataSource;
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     /**
@@ -24,14 +18,9 @@ public class UserDao {
      *
      * @param user 유저 정보
      */
-    public void add(User user) throws SQLException {
-        jdbcContext.workWithStatementStrategy(con -> {
-            PreparedStatement pstmt = con.prepareStatement("insert into users(id, name, password) values (?, ?, ?)");
-            pstmt.setString(1, user.getId());
-            pstmt.setString(2, user.getName());
-            pstmt.setString(3, user.getPassword());
-            return pstmt;
-        });
+    public void add(User user) {
+        jdbcTemplate.update("insert into users(id, name, password) values (?, ?, ?)",
+                user.getId(), user.getName(), user.getPassword());
     }
 
     /**
@@ -40,72 +29,23 @@ public class UserDao {
      * @param id 유저 아이디
      * @return 조회된 유저 정보
      */
-    public User get(String id) throws SQLException {
-        Connection con = dataSource.getConnection();
-        PreparedStatement pstmt = con.prepareStatement("select * from users where id = ?");
-        pstmt.setString(1, id);
-
-        ResultSet rs = pstmt.executeQuery();
-        User user = null;
-        if (rs.next()) {
-            user = new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
-        }
-
-        rs.close();
-        pstmt.close();
-        con.close();
-
-        if (user == null) {
-            throw new EmptyResultDataAccessException(1);
-        }
-
-        return user;
+    public User get(String id) {
+        return jdbcTemplate.queryForObject("select * from users where id = ?",
+                (rs, rowNum) -> new User(rs.getString("id"), rs.getString("name"), rs.getString("password")),
+                id);
     }
 
     /**
      * 유저 전체 삭제
      */
-    public void deleteAll() throws SQLException {
-        jdbcContext.executeSql("delete from users");
+    public void deleteAll() {
+        jdbcTemplate.update("delete from users");
     }
 
-    private StatementStrategy executeSql(final String query) {
-        return con -> con.prepareStatement(query);
-    }
-
-    public int getCount() throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-
-        try {
-            con = dataSource.getConnection();
-            pstmt = con.prepareStatement("select count(*) from users");
-
-            rs = pstmt.executeQuery();
-            rs.next();
-            return rs.getInt(1);
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException e) {
-                }
-            }
-            if (pstmt != null) {
-                try {
-                    pstmt.close();
-                } catch (SQLException e) {
-                }
-            }
-            if (con != null) {
-                try {
-                    con.close();
-                } catch (SQLException e) {
-                }
-            }
-        }
+    /**
+     * 유저 수 조회
+     */
+    public int getCount() {
+        return jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
     }
 }
