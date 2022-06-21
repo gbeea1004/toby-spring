af2886485d5818c6f1e45edf5500f83c88a07471
my <jvckigh@gmail.com>
2022-06-21 23:46:08 +0900

    User 테스트 코드 추가

--- src/main/java/geon/hee/tobyspring/domain/User.java
+++ src/main/java/geon/hee/tobyspring/domain/User.java
@@ -2,9 +2,11 @@ package geon.hee.tobyspring.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class User {
 
---
+++ src/test/java/geon/hee/tobyspring/domain/UserTest.java
@@ -0,0 +1,43 @@
+package geon.hee.tobyspring.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class UserTest {
+
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        user = new User();
+    }
+
+    @Test
+    void upgradeLevel() {
+        Level[] levels = Level.values();
+        for (Level level : levels) {
+            if (level.nextLevel() == null) {
+                continue;
+            }
+            user.setLevel(level);
+            user.upgradeLevel();
+            assertThat(user.getLevel()).isEqualTo(level.nextLevel());
+        }
+    }
+
+    @Test
+    void cannotUpgradeLevel() {
+        Level[] levels = Level.values();
+        for (Level level : levels) {
+            if (level.nextLevel() != null) {
+                continue;
+            }
+            user.setLevel(level);
+            assertThatThrownBy(() -> user.upgradeLevel()).isInstanceOf(IllegalStateException.class);
+        }
+    }
+}
